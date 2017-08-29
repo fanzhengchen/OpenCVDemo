917182b173282c2288bbe501dfe3ff0260e78e2c
fanzhengchen <fanzhengchen@gmail.com>
2017-08-29 12:29:55 +0800

    other demos

--- openCVLibrary330/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary330/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/fanzhengchen/Desktop/OpenCVDemo/openCVLibrary330/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Administrator\\Desktop\\OpenCVDemo\\openCVLibrary330\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
